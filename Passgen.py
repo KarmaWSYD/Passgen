85cc823b5ac05a439a5a963f5b235e9c24882e5b
KarmaWSYD <powertuukka@gmail.com>
2024-11-21 11:20:29 +0200

    modified:   Passgen.py

--- Passgen.py
+++ Passgen.py
@@ -1,6 +1,10 @@
+# Simple password generator that creates a random password of a given length (default 28) with characters from a-z, A-Z, 0-9, and special characters !@#$%/.;:-_*=+?
+# Using python's secrets module https://docs.python.org/3/library/secrets.html#module-secrets instead of random module for better security
+
 import secrets
 
-def passgen(length):
+
+def passgen(length = 28):
     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%/.;:-_*=+?"
     password = ""
     for i in range(length):
@@ -8,5 +12,5 @@ def passgen(length):
     return password
 
 if __name__ == "__main__":
-    print(passgen(28))
+    print(passgen())
     
